b7876ad2f98661ee8d010f2c90e13b9416e7a294
Nicolas Molina Monroy <hi@nicobytes.com>
2022-03-07 12:37:24 -0400

    :loud_sound: Add comments using AAA

--- demos/src/06-person.test.js
+++ demos/src/06-person.test.js
@@ -2,13 +2,17 @@ const Person = require('./06-person');
 
 describe('Test for Person', () => {
   let person;
+  // Arrange
   beforeEach(() => {
     person = new Person('Nicolas', 45, 1.7);
   });
 
   test('should return down', () => {
+    // Arrange
     person.weight = 45;
+    // Act
     const imc = person.calcIMC();
+    // Assert
     expect(imc).toBe('down');
   });
 
