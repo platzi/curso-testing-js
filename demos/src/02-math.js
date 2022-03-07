7c794d4a43c1ce3dff2edfdf6c90820dd8526fb5
Nicolas Molina Monroy <hi@nicobytes.com>
2022-03-07 09:49:31 -0400

    :construction: test for math

---
+++ demos/src/02-math.js
@@ -0,0 +1,20 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    return null;
+  }
+  return a / b;
+}
+
+module.exports = {
+  sum,
+  multiply,
+  divide
+};
---
+++ demos/src/02-math.test.js
@@ -0,0 +1,25 @@
+const { sum, multiply, divide } = require('./02-math');
+
+test("adds 1 + 3 should be 4", () => {
+  const rta = sum(1,3);
+  expect(rta).toBe(4);
+});
+
+test("should be 4", () => {
+  const rta = multiply(1,4);
+  expect(rta).toBe(4);
+});
+
+test("should divide", () => {
+  const rta = divide(6,3);
+  expect(rta).toBe(2);
+  const rta2 = divide(5,2);
+  expect(rta2).toBe(2.5);
+});
+
+test("should divide for zero", () => {
+  const rta = divide(6,0);
+  expect(rta).toBeNull();
+  const rta2 = divide(5,0);
+  expect(rta2).toBeNull();
+});
