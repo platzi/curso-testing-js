ccadc70010c41fdb6ce9fa0b49fb5a26ec7f8069
Nicolas Molina Monroy <hi@nicobytes.com>
2022-03-07 10:29:13 -0400

    :construction: work in asserts

---
+++ demos/src/04-assertions.test.js
@@ -0,0 +1,31 @@
+// matchers
+test('test obj', () => {
+  const data = { name: 'nico' };
+  data.lastname = 'molina';
+  expect(data).toEqual({ name: 'nico', lastname: 'molina' });
+});
+
+test('null', () => {
+  const data = null;
+  expect(data).toBeNull();
+  expect(data).toBeDefined();
+  expect(data).not.toBeUndefined();
+});
+
+test('booleans', () => {
+  expect(true).toEqual(true);
+  expect(false).toEqual(false);
+
+  expect(0).toBeFalsy();
+  expect('').toBeFalsy();
+  expect(false).toBeFalsy();
+});
+
+test('string', () => {
+  expect('Christoph').toMatch(/stop/);
+});
+
+test('list / arrays', () => {
+  const numbers = [1, 2, 3, 4];
+  expect(numbers).toContain(3);
+});
