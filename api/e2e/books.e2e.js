cc2cde92b96dfb7953e1ca8d2ad5658335aba316
Nicolas Molina Monroy <hi@nicobytes.com>
2022-03-08 12:02:43 -0400

    :construction: work

---
+++ api/e2e/books.e2e.js
@@ -0,0 +1,42 @@
+const request = require('supertest');
+
+const { generateManyBook } = require('../src/fakes/book.fake');
+
+const mockGetAll = jest.fn();
+
+jest.mock('../src/lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
+  getAll: mockGetAll,
+  create: () => {},
+})));
+
+const createApp = require('../src/app');
+
+describe('Test for books', () => {
+  let app = null;
+  let server = null;
+  beforeAll(() => {
+    app = createApp();
+    server = app.listen(3001);
+  });
+
+  afterAll(async () => {
+    await server.close();
+  });
+
+  describe('test for [GET] /api/v1/books', () => {
+    test('should return a list books', () => {
+      // Arrange
+      const fakeBooks = generateManyBook(3);
+      mockGetAll.mockResolvedValue(fakeBooks);
+      // Act
+      return request(app)
+        .get('/api/v1/books')
+        .expect(200)
+        .then(({ body }) => {
+          console.log(body);
+          // Assert
+          expect(body.length).toEqual(fakeBooks.length);
+        });
+    });
+  });
+});
