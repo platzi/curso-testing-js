c0270a0d19d4550a4ebf340d24b6b1b472cc34e0
Nicolas Molina Monroy <hi@nicobytes.com>
2022-03-08 10:07:21 -0400

    :construction: mocking

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -1,9 +1,24 @@
 const BooksService = require('./books.service');
 
+const fakeBooks = [
+  {
+    _id: 1,
+    name: 'Harry Potter',
+  },
+];
+
+const MongoLibStub = {
+  getAll: () => [...fakeBooks],
+  create: () => {},
+};
+
+jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => MongoLibStub));
+
 describe('Test for BooksService', () => {
   let service;
   beforeEach(() => {
     service = new BooksService();
+    jest.clearAllMocks();
   });
 
   describe('test for getBooks', () => {
@@ -13,7 +28,16 @@ describe('Test for BooksService', () => {
       const books = await service.getBooks({});
       console.log(books);
       // Assert
-      expect(books.length).toEqual(2);
+      expect(books.length).toEqual(1);
+    });
+
+    test('should return a list book', async () => {
+      // Arrange
+      // Act
+      const books = await service.getBooks({});
+      console.log(books);
+      // Assert
+      expect(books[0].name).toEqual('Harry Potter');
     });
   });
 });
