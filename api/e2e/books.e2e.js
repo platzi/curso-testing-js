01decc9cfad51c3f53ee47f53abb0d3ef134841a
Nicolas Molina Monroy <hi@nicobytes.com>
2022-03-08 12:46:14 -0400

    :white_check_mark: e2e

--- api/e2e/books.e2e.js
+++ api/e2e/books.e2e.js
@@ -1,33 +1,49 @@
 const request = require('supertest');
-
-const { generateManyBook } = require('../src/fakes/book.fake');
-
-const mockGetAll = jest.fn();
-
-jest.mock('../src/lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
-  getAll: mockGetAll,
-  create: () => {},
-})));
+const { MongoClient } = require('mongodb');
 
 const createApp = require('../src/app');
+const { config } = require('../src/config');
+
+const DB_NAME = config.dbName;
+const MONGO_URI = config.dbUrl;
 
 describe('Test for books', () => {
   let app = null;
   let server = null;
-  beforeAll(() => {
+  let database = null;
+
+  beforeAll(async () => {
     app = createApp();
     server = app.listen(3001);
+    const client = new MongoClient(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    await client.connect();
+    database = client.db(DB_NAME);
   });
 
   afterAll(async () => {
     await server.close();
+    await database.dropDatabase();
   });
 
   describe('test for [GET] /api/v1/books', () => {
-    test('should return a list books', () => {
+    test('should return a list books', async () => {
       // Arrange
-      const fakeBooks = generateManyBook(3);
-      mockGetAll.mockResolvedValue(fakeBooks);
+      const seedData = await database.collection('books').insertMany([
+        {
+          name: 'Book1',
+          year: 1998,
+          author: 'nicolas',
+        },
+        {
+          name: 'Book2',
+          year: 1998,
+          author: 'nicolas',
+        },
+      ]);
+      console.log(seedData);
       // Act
       return request(app)
         .get('/api/v1/books')
@@ -35,7 +51,7 @@ describe('Test for books', () => {
         .then(({ body }) => {
           console.log(body);
           // Assert
-          expect(body.length).toEqual(fakeBooks.length);
+          expect(body.length).toEqual(seedData.insertedCount);
         });
     });
   });
